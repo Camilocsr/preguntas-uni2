1b8b9c9138fa73897b6199c0e009db921655dda5
csr <esmunred@gmail.com>
2023-01-13 10:10:11 -0500

    principal

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import preguntas from "./preguntas"
+import { useState, useEffect } from "react"
+function App() {
+  const [preguntaActual, setPreguntaActual] = useState(0);
+  const [puntuacon, setPuntuacion]= useState(0);
+  const [isFinished, setIsFinished] = useState(false);
+  const [tiempoRestante, setTiempoRestante] = useState(100);
+  const [areDisabled, setAreDisabled] = useState (false);
+
+  function handleAnswerSubmit(isCorret,e){
+    if(isCorret == true) {
+      setPuntuacion(puntuacon +1);
+      console.log(puntuacon)
+    }
+    e.target.classList.add(isCorret ? "correct": "incorrect");
+    setTimeout(()=>{
+      if (preguntaActual == preguntas.length -1){
+        setIsFinished(true);
+      } else {
+        setPreguntaActual(preguntaActual + 1);
+      }
+    },700);
+  }
+
+
+  // useEffect(()=>{
+  //   const intervalo = setInterval(()=>{
+
+  //     if(tiempoRestante > 0) setTiempoRestante((tiempo)=>tiempo - 1);
+  //     if(tiempoRestante == 0) setAreDisabled(true);
+
+  //     return ()=> clearInterval(intervalo);
+
+  //   },1000);
+
+  // },[tiempoRestante]);
+
+
+  if(isFinished) return(
+    <main className="app">
+      <div className="juego-terminado">
+        <span>tubiste {puntuacon} de {preguntas.length}{" "}
+        <button onClick={()=>window.location.href="index.html"}>Volver a jugar</button></span>
+      </div>
+    </main>
+  )
+  return (
+  <div className="container-fluid">
+    <main className="app row">
+      <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6 lado-izquierdo">
+    {/* <div className="nuero-pregunta">
+      <span>pregunta numero {preguntaActual} de </span>{preguntas.length}
+    </div> */}
+    <div className="titulo-pregunta">
+      {preguntas[preguntaActual].titulo}
+    </div>
+    {/* <div>
+      <span className="tiempo-restante">Tiempo restante: {tiempoRestante}</span>
+    </div> */}
+      </div>
+      <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6lado-derecho">
+        {preguntas[preguntaActual].opciones.map((respuesta)=>(
+          <button disabled={areDisabled} onClick={(e)=>handleAnswerSubmit(respuesta.isCorret,e)} key={respuesta.textoRespuesta}>{respuesta.textoRespuesta}</button>
+        ))}
+      </div>
+    </main>
+  </div>
+)
+}
+
+export default App
---
+++ src/preguntas.js
@@ -0,0 +1,44 @@
+const preguntas = [
+    {
+        titulo : "¿Que es una unidad de disco duro?",
+        opciones: [
+            {textoRespuesta: "almacenador de memoria independiente..", isCorret: false},
+            {textoRespuesta: "almacenador de datos temporal a corto plazo.", isCorret: false},
+            {textoRespuesta: "El disco duro es un dispositivo de memoria secundaria no volátil, es decir, guarda la información, programas, controladores y el sistema operativo del computador", isCorret: true}
+        ]
+    },
+    {
+        titulo : "¿Que es un disco duro SSD?",
+        opciones: [
+            {textoRespuesta: "unidad de almacenamiento por discos.", isCorret: false},
+            {textoRespuesta: "Son discos duros como si fueran memoria ram pero no son volátiles Son discos duros como si fueran memoria ram pero son volátiles.", isCorret: true},
+            {textoRespuesta: "acronimo de SATA Save distribution", isCorret: false}
+        ]
+    },
+    {
+        titulo : "¿Qué componentes básicos conforman un Disco Duro?",
+        opciones: [
+            {textoRespuesta: "Discos paralelos, cabezales de lectura/escritura", isCorret: true},
+            {textoRespuesta: "disco, motor, lector, chip de datos.", isCorret: false},
+            {textoRespuesta: "disco, motor, lector, ventilador", isCorret: false}
+        ]
+    },
+    {
+        titulo : "¿Qué es una partición?",
+        opciones: [
+            {textoRespuesta: "Partes fisicas principal del disco interno.", isCorret: false},
+            {textoRespuesta: "espaxcio de memoria dedicado a la ram", isCorret: false},
+            {textoRespuesta: "Una partición es una división lógica de un disco duro que es interpretada por el sistema operativo como un disco independiente, cada partición del disco duro puede tener un sistema de archivos, un sistema operativo o sus propios documentos y archivos.", isCorret: true}
+        ]
+    },
+    {
+        titulo : "¿Cual de estos tipos de disco duro es verdadero..?",
+        opciones: [
+            {textoRespuesta: "Disco jpk", isCorret: false},
+            {textoRespuesta: "Disco swap", isCorret: false},
+            {textoRespuesta: "discos de estado sólido.", isCorret: true}
+        ]
+    }
+]
+
+export default preguntas
